cad74f13d790be3478185ad2f6cd8f91fc2d12ee
lemasc <lemascserviceth@hotmail.com>
2017-12-03 13:17:12 +0700

    Update Github Pages #1

---
+++ docs/lang/en/form.php
@@ -0,0 +1,28 @@
+<?php 
+$vst_headertext[0] = "VasiTutor Form Registration";
+$vst_text[0] = "We're glad that you contribute with the VasiTutor Community!<br>
+You will be one of the very important people that behinds the world's education.<br>";
+$vst_text[1] = "In other to continue to VasiTutor Community, please complete the form to continue.";
+$vst_text[2] = 'By clicking "Register", you are agree with ';
+$vst_text[3] = "VasiTutor Terms and Conditions";
+$vst_text[4] = "Register";
+$vst_text[5] = "All fields are required except the comments.";
+$vstform_legend[0] = "Personal and Login Information";
+$vstform_legend[1] = "What is your purpose to register VasiTutor Community?";
+$vstform_label[0] = "Name";
+$vstform_label[1] = "Surname";
+$vstform_label[2] = "Telephone No";
+$vstform_label[3] = "Email";
+$vstform_label[4] = "Username";
+$vstform_label[5] = "Password";
+$vstform_label[6] = "Repeat password";
+$vstform_label[7] = "I would like to..";
+$vstform_label[8] = "Some comments to us..";
+$vstform_label[9] = "Name Title";
+$vstform_value[0] = "Choose...";
+$vstform_value[1] = "Mr.";
+$vstform_value[2] = "Mrs.";
+$vstform_value[3] = "Develop and improve VasiTutor website";
+
+
+?>
---
+++ docs/lang/en/index.php
@@ -0,0 +1,18 @@
+<?php 
+$vst_headertext[0] = "Welcome to VasiTutor Open Project";
+$vst_headertext[1] = "We are not a spam, and we don't stop our mission. We have a real contact information for you.";
+$vst_headertext[2] = "Contact Information";
+$vst_text[0] = "VasiTutor Open Project is a project that have a mission to provide a free learning media, contents, practices and quizes<br>
+Everyone can learn, test and educate themselves anywhere, anytime.<br>";
+$vst_text[1] = "VasiTutor is in beta and not a public-release yet. Our team are now working hard to provide VasiTutor as fast as we can.<br>
+Support us by help contributing our website with no cost or donate to our account.";
+$vst_text[2] = "Start contributing our website";
+$vst_text[3] = "or";
+$vst_text[4] = "donate method.";
+$vst_text[5] = "169/19 Moo 1 Tesaban 2 Rd. Pimolrad. Bangbuathong, Nonthaburi, Thailand 11100<br>
+Email:";
+$vst_text[6] = "vasitutorth@yahoo.com";
+$vst_text[7] = "<br>Call: (66+)866334848";
+$vst_text[8] = "Form Registration";
+$vst_text[9] = "Donation Method";
+?>
